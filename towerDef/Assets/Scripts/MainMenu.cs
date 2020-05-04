0c05e8f2579b45b5931e6dcc1e0ad193a484dda2
legyllith <jeremy.n.49@live.fr>
2020-05-04 20:30:15 +0200

    main menu

---
+++ towerDef/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string LevelToLoad = "Scenes"+"/"+"scene1";
+
+    public void Play()
+    {
+        Debug.Log(LevelToLoad);
+        SceneManager.LoadScene(LevelToLoad);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Fermeture du jeu");
+        Application.Quit();
+    }
+}
