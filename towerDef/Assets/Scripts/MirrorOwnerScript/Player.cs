ef7642669f8ab49c35668e7f851bf33951cdac08
legyllith <jeremy.n.49@live.fr>
2020-05-12 20:56:18 +0200

    test mirror

---
+++ towerDef/Assets/Scripts/MirrorOwnerScript/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Mirror.test.owner
+{
+    public class Player : NetworkBehaviour
+    {
+        [SerializeField] private Vector3 movement = new Vector3();
+
+        [Client]
+        private void Update()
+        {
+            if (!hasAuthority) { return; }
+
+            if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+            
+            CmdMove();
+        }
+
+        [Command]
+        private void CmdMove()
+        {
+           RpcMove();
+        }
+
+        [ClientRpc]
+        private void RpcMove() => transform.Translate(movement);
+    }
+}
+
